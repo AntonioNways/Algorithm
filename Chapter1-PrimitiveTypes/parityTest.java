8a619806fd52bea38b45589b75aa72902e32828f
Silver Dn <Antonion90@gmail.com>
2018-09-09 22:53:55 -0400

    refactor parity test

--- Chapter1-PrimitiveTypes/parityTest.java
+++ Chapter1-PrimitiveTypes/parityTest.java
@@ -5,8 +5,8 @@ public class parityTest {
     public static short parity(long x){
     	short result = 0;
     	while ( x !=0){
-    		result ^= (x & 1);
-    		x >>>= 1;
+    		result ^= 1;
+    		x &=(x-1);
     	}
     	return result;
     }	
