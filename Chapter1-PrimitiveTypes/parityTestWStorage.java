18f3bb52ea3e540fca21703816409285535e1d9e
Silver Dn <Antonion90@gmail.com>
2018-09-24 00:23:28 -0400

    add fillParity

--- Chapter1-PrimitiveTypes/parityTestWStorage.java
+++ Chapter1-PrimitiveTypes/parityTestWStorage.java
@@ -1,25 +1,37 @@
-import java.util.Scanner;
+import java.util.*;  
 
 public class parityTestWStorage {
 
     public static short parity(long x){
-    	short result = 0;
-    	while ( x !=0){
-    		result ^= 1;
-    		x &=(x-1);
-    	}
-    	return result;
-    }	
+        short result = 0;
+        while ( x !=0){
+            result ^= 1;
+            x &=(x-1);
+        }
+        return result;
+    }
+
+    public static short fillParity(long args[]){
+        HashMap<Short, Short> tableMap = new HashMap<Short, Short>();
+    	for(int counter = 0; counter < 131071; counter++){
+    		tableMap.put(counter, parity(counter))
+	    }    
+    }   
 
   public static void main(String args[]) {
-	Scanner user_input = new Scanner( System.in );
+    Scanner user_input = new Scanner( System.in );
+    
 
-	short inputParity;
-	System.out.print(parity(inputParity));
-
-	    System.out.print("Enter your interger check or type exit to leave:");
-	    inputParity = user_input.next();
-	    System.out.print(parity(inputParity));
-   
+    String inputParity;
+    boolean checkInput = true;
+    String message = "Enter your interger check, anything else will exit: \n";
+    while (checkInput){
+        System.out.print(message);
+        checkInput = user_input.hasNextShort();
+        inputParity = user_input.next();
+        if(checkInput) System.out.println("Parity value = " + parity(Short.parseShort(inputParity)));
+        
+    }
+   }
    
 }
