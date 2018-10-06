c5ee1f1c5e98122feb243d927e9308f469f5aec4
Silver Dn <Antonion90@gmail.com>
2018-10-06 16:44:14 -0400

    update parityTestWStorage

--- Chapter1-PrimitiveTypes/parityTestWStorage.java
+++ Chapter1-PrimitiveTypes/parityTestWStorage.java
@@ -2,8 +2,17 @@ import java.util.*;
 
 public class parityTestWStorage {
 
-    public static short parity(long x){
-        short result = 0;
+    //constructor
+	public parityTestWStorage(){
+        for(int counter = 0; counter < 131071; counter++){
+            this.parityTable.put(counter, parity(counter));
+        }     
+	}	
+
+
+  private HashMap<Integer, Integer> parityTable = new HashMap<Integer, Integer>();
+    public static int parity(long x){
+        int result = 0;
         while ( x !=0){
             result ^= 1;
             x &=(x-1);
@@ -11,41 +20,26 @@ public class parityTestWStorage {
         return result;
     }
 
-    public static short fillParity(long args[]){
-        HashMap<Short, Short> parityTable = new HashMap<Short, Short>();
-    	for(int counter = 0; counter < 131071; counter++){
-    		parityTable.put(counter, parity(counter))
-	    }   
-	    return parityTable; 
-    }   
+    private void printMsg (){
+	    System.out.println(parityWithTable(12)); 	
+    }
 
-    public static short parityWithTable(long x){
-    HashMap<Short, Short> parityTable = fillParity();
-    final int bitMaskTo16 = 0XFFFF;
-    //assume that value goes up to 2^64 then we need to split the number into 4 separate section
-    final int first = parityTable.get((int)(x & bitMaskTo16)); 
-    final int second = parityTable.get((int)((x>>>16) & bitMaskTo16));
-    final int third = parityTable.get((int)(x>>>32) & bitMaskTo16));
-    final int forth = parityTable.get((int)(x>>>48) & bitMaskTo16));
+
+    public int parityWithTable(long x){
+    long bitMaskTo16 = 0XFFFF;
+    // //assume that value goes up to 2^64 then we need to split the number into 4 separate section	
+    int first = parityTable.get((int)(x & bitMaskTo16));
+    int second = parityTable.get((int)((x>>>16) & bitMaskTo16));
+    int third = parityTable.get((int)((x>>>32) & bitMaskTo16));
+    int forth = parityTable.get((int)((x>>>48) & bitMaskTo16));
 	return first ^ second ^ third ^ forth;
 	}
+
   public static void main(String args[]) {
+    parityTestWStorage parity = new parityTestWStorage();
+    parity.printMsg();    
+		   
 
-
-
-    /*Scanner user_input = new Scanner( System.in );
-    
-
-    String inputParity;
-    boolean checkInput = true;
-    String message = "Enter your interger check, anything else will exit: \n";
-    while (checkInput){
-        System.out.print(message);
-        checkInput = user_input.hasNextShort();
-        inputParity = user_input.next();
-        if(checkInput) System.out.println("Parity value = " + parity(Short.parseShort(inputParity)));
-        
-    }
-   }*/
+   }
    
 }
