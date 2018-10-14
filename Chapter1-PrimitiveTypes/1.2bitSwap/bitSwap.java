4cf78203021bae1ebcb8434eba734572720c07bc
Silver Dn <Antonion90@gmail.com>
2018-10-13 22:02:36 -0400

    move bitSwap

--- Chapter1-PrimitiveTypes/1.2bitSwap/bitSwap.java
+++ Chapter1-PrimitiveTypes/1.2bitSwap/bitSwap.java
@@ -7,6 +7,7 @@ public class bitSwap {
 
     private static long printMsg (long x, int i, int j){	
         if(((x>>i)&1) != ((x>>j)&1)){
+            
             x = x^(1<<i)^(1<<j);
         }
         return x;
@@ -16,7 +17,9 @@ public class bitSwap {
 
   public static void main(String args[]) { 
 
-    System.out.println(printMsg(10, 0, 1));   
+    System.out.println(printMsg(19, 0, 1));   
+    System.out.println(printMsg(19, 0, 2));   
+    System.out.println(printMsg(19, 3, 2));   
 		   
 
    }
