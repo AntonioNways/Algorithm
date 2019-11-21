091930001b84a463758161e41fc8839efbc69a05
AntonioNways <siiv3rd@gmail.com>
2019-11-21 18:15:33 -0500

    add ;

--- JavaScript/TwoSum.js
+++ JavaScript/TwoSum.js
@@ -1,12 +1,14 @@
 // Problem:
 //Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
+
 var twoSum = function(nums, target) {
-    let record = {}
+    let record = {};
     for(var i= 0; i<nums.length; i++){
         if(record[target - nums[i]] !== undefined) return [record[target - nums[i]],i];
         record[nums[i]]=i;
