b4dc61d85e7879b8f894777f07f9238be7e0b47d
AntonioNways <antonion90@gmail.com>
2019-11-21 19:03:13 -0500

    add zigzag

---
+++ JavaScript/ZigZagConversion.js
@@ -0,0 +1,6 @@
+//Problem:
+//The string "SAYPALIQHITING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+// S   A   H   N
+// A P L Q I I G
+// Y   I   T
+//And then read line by line: "SAHNAPLQIIGYIT"
